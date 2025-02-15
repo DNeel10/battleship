9ebb9f27a02816ba4b5a14d89b7fdbe81a7c1fd4
Daniel Neel <dneel10@gmail.com>
2025-02-14 22:22:25 -0500

    built ship and ship tests

--- src/__tests__/ship.test.js
+++ src/__tests__/ship.test.js
@@ -7,7 +7,31 @@ describe("ship module", () => {
   test("the function returns an object", () => {
     expect(createShip()).toBeInstanceOf(Object);
   });
-  test("the returned object has the length property", () => {
-    expect(createShip(4)).toHaveProperty("length", 4);
+  test("the created ship object has a hitsRemaining property", () => {
+    const ship = createShip(4);
+
+    expect(ship).toHaveProperty("hitsRemaining", 4);
+  });
+  test("the created ship object has a hit() function", () => {
+    const ship = createShip(4);
+
+    expect(ship).toHaveProperty("hit");
+  });
+  test("the hit function increments the 'hit' property of the object", () => {
+    const ship = createShip(4);
+    ship.hit();
+
+    expect(ship.hitsRemaining).toBe(3);
+  });
+  test("the ship isSunk when hitsRemaining falls to 0", () => {
+    const ship = createShip(2);
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+  test("the isSunk method returns false if the ship has hits remaining", () => {
+    const ship = createShip(2);
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
   });
 });
--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,19 @@
 export default function createShip(length) {
+  let hitsRemaining = length;
+
+  function hit() {
+    hitsRemaining--;
+  }
+
+  function isSunk() {
+    return hitsRemaining === 0;
+  }
+
   return {
-    length,
+    hit,
+    get hitsRemaining() {
+      return hitsRemaining;
+    },
+    isSunk,
   };
 }
